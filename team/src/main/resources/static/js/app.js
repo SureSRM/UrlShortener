e588019eeaa6ce7c5c15999e961c86692afe4597
SureSRM <sergiozz95@gmail.com>
2016-11-14 21:02:28 +0100

    Add frontend representation of info in a semi-fancy format

--- team/src/main/resources/static/js/app.js
+++ team/src/main/resources/static/js/app.js
@@ -15,7 +15,40 @@ $(document).ready(
                         + " hits.</p></div>"
                     );
                 }
+            });$.ajax('http://localhost:8080/metrics', 'GET').done(
+            function(data) {
+                console.log(data);
+                $("#uptime").append( data["uptime"] );
+                $("#mem").append( data["mem"] );
+                $("#memused").append( data["mem.free"] );
+                $("#avload").append( data["systemload.average"] );
+
+                $("#countshorted").append( data["counter.status.201.link"] );
+                $("#countaccess").append( data["counter.status.304.star-star"] );
+                $("#avacessperurl").append( data["counter.status.201.link"] / data["counter.status.304.star-star"] );
+                $("#timelastredirection").append( data["gauge.response.star-star"] );
             });
+
+        // Get the modal
+        var modal = document.getElementById('infoModal');
+        // Get the button that opens the modal
+        var btn = document.getElementById('infoButton');
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        // When the user clicks on the button, open the modal
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
         $("#shortener").submit(
             function(event) {
                 event.preventDefault();
